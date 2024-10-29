9e565497e9627f934612c93de2daf7e53b37e6ff
songa <thddk0121@gmail.com>
2024-10-29 15:18:57 +0900

    style: 인트로 애니메이션 효과 css 적용

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,13 +1,16 @@
 import styled from "styled-components";
 import IntroTextStyle from "../styles/introTextStyle";
+import { fadeInBold, fadeInSoft, fadeOutBold, fadeOutSoft, shrinkToCircle } from "../styles/animations";
 
-const IntroWraper = styled.div`
+const IntroWrapper = styled.div`
   width: 100%;
   height: 100%;
   inset: 0;
   position: fixed;
   z-index: 999;
+
   background-color: var(--primary-color);
+  animation: ${shrinkToCircle} 3s 4.5s ease-in-out forwards;
 `;
 
 const IntroTextWrapper = styled.div`
@@ -20,33 +23,47 @@ const IntroTextWrapper = styled.div`
   text-align: center;
   color: #fff;
 
-  span {
+  > span {
     font-family: "Montserrat", sans-serif;
     font-size: 9.5rem;
     font-weight: 800;
     letter-spacing: -2px;
-    opacity: 0.2;
+    opacity: 0;
+    animation: ${fadeInSoft} 2s ease-in-out forwards, ${fadeOutSoft} 2s 3s linear forwards;
   }
 `;
 
 const StyledIntroH1 = styled.h1`
   ${IntroTextStyle}
 
+  display: flex;
+  flex-direction: column;
   margin-top: -60px;
+  font-size: 2.75rem;
+
+  > span:nth-child(1) {
+    opacity: 0;
+    animation: ${fadeInBold} 1.5s 0.5s ease-in-out forwards, ${fadeOutBold} 1.5s 2.5s ease-in-out forwards;
+  }
+  > span:nth-child(2) {
+    opacity: 0;
+    animation: ${fadeInBold} 1.5s 1s ease-in-out forwards, ${fadeOutBold} 1.5s 3s ease-in-out forwards;
+  }
 `;
 
 function Intro() {
   return (
-    <IntroWraper>
+    <IntroWrapper>
       <IntroTextWrapper>
         <span>Front-End</span>
         <StyledIntroH1>
-          함께 협력하고 같이 성장하는
-          <br />
-          프론트엔드 개발자 <b>이송아</b>입니다.
+          <span>함께 협력하고 같이 성장하는</span>
+          <span>
+            프론트엔드 개발자 <b>이송아</b>입니다.
+          </span>
         </StyledIntroH1>
       </IntroTextWrapper>
-    </IntroWraper>
+    </IntroWrapper>
   );
 }
 
---
+++ src/styles/animations.ts
@@ -0,0 +1,71 @@
+import { keyframes } from "styled-components";
+
+// 인트로 애니메이션
+const shrinkToCircle = keyframes`
+	0% {
+		width: 100vw;
+		height: 100vw;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
+	50% {
+		border-radius: 500px;
+		top: 20%;
+		left: 60%;
+	}
+	100% {
+		width: 95px;
+		height: 95px;
+		position: absolute;
+		top: 37%;
+		left: 65%;
+		border-radius: 999px;
+	}
+`;
+
+const fadeInSoft = keyframes`
+	0% {
+		opacity: 0;
+	}
+	50% {
+		opacity: 0.1;
+		
+	}
+	100% {
+		opacity: 0.2;
+	}
+`;
+
+const fadeInBold = keyframes`
+	0% {
+		opacity: 0;
+	}
+	50% {
+		opacity: 0.5;
+		
+	}
+	100% {
+		opacity: 1;
+	}
+`;
+
+const fadeOutSoft = keyframes`
+	0% {
+		opacity: 0.2;
+	}
+	100% {
+		opacity: 0;
+	}
+`;
+
+const fadeOutBold = keyframes`
+	0% {
+		opacity: 1;
+	}
+	100% {
+		opacity: 0;
+	}
+`;
+
+export { shrinkToCircle, fadeInSoft, fadeInBold, fadeOutSoft, fadeOutBold };
