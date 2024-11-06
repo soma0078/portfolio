c6823bc8013858268672d5655ad0af8b61ac1a3a
soma0078 <104878713+soma0078@users.noreply.github.com>
2024-11-06 23:44:28 +0900

    feat: 팝업 오픈 시 배경 스크롤 방지 기능 구현

--- src/components/common/PopupLayout.tsx
+++ src/components/common/PopupLayout.tsx
@@ -1,6 +1,6 @@
 import { MdClose } from "react-icons/md";
 import styled from "styled-components";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 
 interface PopupProps {
   onClose: () => void;
@@ -56,6 +56,19 @@ const CloseButton = styled.button`
 `;
 
 function PopupLayout({ onClose, children }: PopupProps) {
+  useEffect(() => {
+    document.body.style.cssText = `
+      position: fixed; 
+      top: -${window.scrollY}px;
+      overflow-y: scroll;
+      width: 100%;`;
+    return () => {
+      const scrollY = document.body.style.top;
+      document.body.style.cssText = "";
+      window.scrollTo(0, parseInt(scrollY || "0", 10) * -1);
+    };
+  }, []);
+
   return (
     <StyledPopupLayout>
       <PopupBackgound onClick={onClose} />
